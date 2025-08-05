45ad6cdcc4caeee32542edebac7d96ff0030f398
Tmacy <chen_tmacy@foxmail.com>
2025-08-05 09:05:11 +0800

    uncomment code

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ fn match_pattern(input_line: &str, pattern: &str) -> bool {
 // Usage: echo <input_text> | your_program.sh -E <pattern>
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
-    eprintln!("Logs from your program will appear here!");
+    // eprintln!("Logs from your program will appear here!");
 
     if env::args().nth(1).unwrap() != "-E" {
         println!("Expected first argument to be '-E'");
@@ -26,9 +26,9 @@ fn main() {
     io::stdin().read_line(&mut input_line).unwrap();
 
     // Uncomment this block to pass the first stage
-    // if match_pattern(&input_line, &pattern) {
-    //     process::exit(0)
-    // } else {
-    //     process::exit(1)
-    // }
+    if match_pattern(&input_line, &pattern) {
+        process::exit(0)
+    } else {
+        process::exit(1)
+    }
 }

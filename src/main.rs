e072aa34b2e20b3f0da4d677feed978b7dd1100b
Tmacy <chen_tmacy@foxmail.com>
2025-08-18 11:33:54 +0800

    match word

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,9 @@ fn match_pattern(input_line: &str, pattern: &str) -> bool {
         log::debug!("count is 1");
         return input_line.contains(pattern);
     } else {
-        input_line.chars().any(|c| c.is_ascii())
+        input_line
+            .chars()
+            .any(|c| c.is_ascii_alphanumeric() || c == '_')
     }
 }
 
